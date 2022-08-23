b3fd3eae6bfcc8e7c65ae68ab8f839cebb3ce2bf
xugao <xugaohero@163.com>
2022-08-23 23:08:24 +0800

    提交 @徐高 finish

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"bookstore/server/middleware"
 	"bookstore/store"
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -13,7 +16,7 @@ type BookStoreServer struct {
 	srv *http.Server
 }
 
-func NewBookStoreServer(addr string, s store.Store) {
+func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
 	srv := &BookStoreServer{
 		s: s,
 		srv: &http.Server{
@@ -23,12 +26,44 @@ func NewBookStoreServer(addr string, s store.Store) {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/book", srv.createBookHandler).Methods("POST")
+	router.HandleFunc("/book/{id}", srv.getBookHandler).Methods("GET")
+	router.HandleFunc("/book/{id}", srv.updateBookHandler).Methods("POST")
+	router.HandleFunc("/book", srv.getAllBookHandler).Methods("GET")
+	router.HandleFunc("/book/{id}", srv.delBookHandler).Methods("DELETE")
+
+	srv.srv.Handler = middleware.Logging(middleware.Validating(router))
+	return srv
+}
+
+func (bs *BookStoreServer) ListenAndServe() (<-chan error, error) {
+	var err error
+	errChan := make(chan error)
+	go func() {
+		err = bs.srv.ListenAndServe()
+		errChan <- err
+	}()
+
+	select {
+	case err = <-errChan:
+		return nil, err
+	case <-time.After(time.Second):
+		return errChan, nil
+	}
+}
+
+func (bs *BookStoreServer) Shutdown(ctx context.Context) error {
+	return bs.srv.Shutdown(ctx)
 }
 
 func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request) {
 	dec := json.NewDecoder(req.Body)
 	var book store.Book
 	if err := dec.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := bs.s.Create(&book); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -47,6 +82,49 @@ func (bs *BookStoreServer) getBookHandler(w http.ResponseWriter, req *http.Reque
 		return
 	}
 	response(w, book)
+}
+
+func (bs *BookStoreServer) updateBookHandler(w http.ResponseWriter, req *http.Request) {
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "no id found in request", http.StatusBadRequest)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	var book store.Book
+	if err := dec.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	book.Id = id
+	if err := bs.s.Update(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func (bs *BookStoreServer) getAllBookHandler(w http.ResponseWriter, req *http.Request) {
+	books, err := bs.s.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response(w, books)
+}
+
+func (bs *BookStoreServer) delBookHandler(w http.ResponseWriter, req *http.Request) {
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "no id found in request", http.StatusBadRequest)
+		return
+	}
+	err := bs.s.Delete(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
 
 func response(w http.ResponseWriter, v interface{}) {
